e750d62ae23b8bf58149d27c85ecd3d1b09062e9
Justin Kapacinskas <dulouz2001@hotmmail.com>
2025-05-19 07:26:59 -0500

    Adds ALLOWED_HOSTS

--- portfolio_be/portfolio_be/settings.py
+++ portfolio_be/portfolio_be/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = 'django-insecure-5+jl%)dl=a=qi97f3&f5n7o&^&n4@gx3zlcf5-dhxi+xlf(n)6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://20.80.81.230'
+]
 
 
 # Application definition
